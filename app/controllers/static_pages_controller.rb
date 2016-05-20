f324e8a6cec04cd9b3946a9563d98ef630f1a218
Liang <liajiaru@u480fcf4157b75720d302.ant.amazon.com>
2016-05-20 23:39:14 +0000

    finished StaticPages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,16 @@
 class StaticPagesController < ApplicationController
   def home
   end
+	# when visiting the URL /static_pages/home, 
+	# Rails looks in the Static Pages controller and executes the code in the home action,
+	# and then renders the view corresponding to the action. 
+
+	# In the present case, the home action is empty, so
+	# visiting /static_pages/home does render the view and does nothing else
 
   def help
+  end
+
+  def about
   end
 end
